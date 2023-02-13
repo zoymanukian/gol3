224d3aadffe30351996c6d748d8f0e93af4f08ec
zoymanukian <zoya.manukyan@gmail.com>
2023-02-13 20:22:39 +0400

    server

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+app.get("/", function (req, res) {
+    res.send("<h1>Hello world</h1>");
+
+});
+
+app.get("/name/:name", function (req, res) {
+    var name = req.params.name;
+    res.send("<h1>Hello " + name + "</h1>");
+
+});
+
+app.listen(3000, function () {
+    console.log("Example is running on port 3000");
+});
