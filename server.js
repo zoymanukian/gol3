e7318c193f541f634c2051011f8107fa583ae741
ArpiG9 <arpi.gyulgyulyan.y@tumo.org>
2023-02-20 20:24:58 +0400

    a

--- server.js
+++ server.js
@@ -1,13 +1,87 @@
-var express = require("express");
+var express = require('express');
+
 var app = express();
+
+var server = require('http').createServer(app);
+
+var io = require('socket.io')(server);
+
+var messages = [];
 
 app.use(express.static("modules"));
 
-app.get("/", function (req, res) {
-    res.redirect("index.html");
+app.get('/', function (req, res) {
+
+res.redirect('index.html');
 
 });
 
-app.listen(3000, function () {
-    console.log("Example is running on port 3000");
+server.listen(3000);
+
+grassArr = [];
+grassEaterArr = [];
+predatorArr = [];
+creatorArr = [];
+fertilizerArr = [];
+energyArr = [];
+matrix = [];
+
+function generateMatrix(matLen, gr, grEat, pr, cre, fer) {
+
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = []
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0
+        }
+    }
+
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 1
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 2
+        }
+    }
+    for (let i = 0; i < pr; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 3
+        }
+    }
+    for (let i = 0; i < cre; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 4
+        }
+    }
+    for (let i = 0; i < fer; i++) {
+        let x = Math.floor(Math.random() * matLen)
+        let y = Math.floor(Math.random() * matLen)
+        if (matrix[y][x] == 0) {
+            matrix[y][x] = 5
+        }
+    }
+    
+
+
+
+}
+
+
+generateMatrix(25, 10, 5, 10, 6, 3, )
+
+io.on('connection', function (socket) {
+    socket.emit("send matrix", matrix);
+
+
 });
